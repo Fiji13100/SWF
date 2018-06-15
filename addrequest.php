6f0a360c93a0394c93e48ea394457d03f958d87a
fiji13100 <flashmob2013@hotmail.fr>
2018-06-15 06:00:22 +0200

    commint css  base

--- addrequest.php
+++ addrequest.php
@@ -4,4 +4,15 @@ require 'libs/functions.php';
 
 debug($_POST);
 
+if(isset($_POST) and
+    isset($_POST['server']) and 
+    isset($_POST['element']) and
+    isset($_POST['monster']) and
+    isset($_POST['timer']) and $_POST['timer'] <= 60 and $_POST['timer'] >= 5 and
+    isset($_POST['pseudo'])) {
+
+    
+    echo 'Test'; 
+}
 ?>
+
